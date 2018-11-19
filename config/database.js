4d01b9c1457c99d4bc71be941fcc4d216f1cd3c6
Patryk <p171982@gmail.com>
2018-11-19 10:00:13 +0100

    FA

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = { mongoURI: 'mongodb://<patryk01>:<patryk01>@ds155823.mlab.com:55823/vidjot-prod2'}
+  module.exports = { mongoURI: 'mongodb://patryk01:patryk01@ds155823.mlab.com:55823/vidjot-prod2'}
 } else {
   module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
